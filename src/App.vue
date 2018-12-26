207c3d43d71a8ea82ef8d3eb0cda9cfcb7b48121
samir elsharkawy <samir.elsharkawy@gmail.com>
2018-12-26 20:56:52 +0100

    initial version of the app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,72 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ul>
+        <li class="navbar-header">
+          <router-link to="/">Welcome Map Annecy</router-link>
+        </li>
+        <li style="float:right">
+          <router-link to="/infos">Infos</router-link>
+        </li>
+      </ul>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+
+  display: flex;
+  flex-direction: column;
+  flex-flow: column;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#nav {
+  padding: 20px;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+#nav li {
+  float: left;
+}
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+  display: block;
+  padding: 0 5px;
+  text-align: center;
+  text-decoration: none;
+}
+
+a:hover:not(.active) {
+  color: #42b983;
+}
+
+#nav li a:hover:not(.active) {
+  background-color: #ebebeb;
 }
 </style>
