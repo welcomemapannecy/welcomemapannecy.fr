fad61e69bdb434959b5483408ffafb1d74863e00
samir elsharkawy <samir.elsharkawy@gmail.com>
2018-12-27 21:45:49 +0100

    update: Info page

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,20 +1,51 @@
 <template>
   <div class="info">
+    <div class="presentation">
+      <h1>Présentation</h1>
+      <p class="info-paragraph" style="max-width: 700px; margin:0 auto;">
+        La Welcome Map est un projet de centralisation des données utiles à
+        destination des exilés arrivés à Annecy. Le projet Welcome Map a débuté
+        de façon bénévole en 2016 à Paris, et a été répliqué dans d'autres
+        villes de France
+      </p>
+    </div>
+    <br />
+    <br />
     <div class="contact">
-      <h1>Contact</h1>
-      <a href="mailto: contact@welcomemapannecy.fr" target="_blank"
-        >Nous envoyer un mail</a
+      <h1>Contribuer, nous contacter</h1>
+      <a href="mailto: contact@welcomemapannecy.fr" target="_blank">
+        contact@welcomemapannecy.fr
+      </a>
+      <a
+        href="https://github.com/welcomemapannecy/welcomemapannecy.fr"
+        target="_blank"
       >
+        GitHub
+      </a>
     </div>
     <br />
     <br />
     <div class="project">
-      <h1>Contribution</h1>
-      <a
-        href="https://github.com/welcomemapannecy/welcomemapannecy.fr"
-        target="_blank"
-        >GitHub</a
-      >
+      <h1>Crédit</h1>
+      <p>
+        Réalisation des cartes par Anne-Elise Lenne ( Fond de carte par
+        <a
+          target="_blank"
+          href="https://www.openstreetmap.org"
+          style="display: inline;"
+        >
+          OpenStreetMap
+        </a>
+        , Cartographie faite sur
+        <a
+          href="http://umap.openstreetmap.fr/fr/"
+          target="_blank"
+          style="display: inline;"
+        >
+          Umap
+        </a>
+        )
+      </p>
     </div>
   </div>
 </template>
